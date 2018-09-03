e6595d1dcb170744678b809b767ebc3dbaccdc23
Shambhavi <reply2112@gmail.com>
2018-09-03 16:27:11 +0530

    Add required code to url_utilities.py

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -1,13 +1,69 @@
+import re
+import string
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
+
 def load_urls_from_files(file_path: str):
-    # ToDo: Add code to read the URL file.
-    pass
+    try:
+        with open(file_path) as f:
+            content = f.readline()
+            return content
+    except FileNotFoundError:
+        print('The file at ' + file_path + ' could not be found.')
+        exit(2)
+        # The exit code 2 indicates that a return value could not be generated or
+        # the command did not run successfully.
 
 
 def load_page(url: str):
-    # ToDo: Add code to read each URL.
-    pass
+    response = urlopen(url)
+    html = response.read().decode('utf-8')
+    return html
 
 
+# page_contents is a string that contains HTML from the pages that the application has read.
 def scrape_page(page_contents: str):
-    # ToDo: Analyze text.
-    pass
+    chicken_noodle = BeautifulSoup(page_contents, "html5lib")
+
+    for script in chicken_noodle(["script", "style"]):
+        script.extract()
+
+    text = chicken_noodle.get_text()
+    lines = (line.strip() for line in text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+
+    text = ' '.join(chunk for chunk in chunks if chunk)
+    plain_text = ''.join(filter(lambda x: x in string.printable, text))
+
+    clean_words = []
+
+    words = plain_text.split(" ")
+
+    for word in words:
+        clean = True
+
+        # no punctuation
+        for punctuation_marks in string.punctuation:
+            if punctuation_marks in word:
+                clean = False
+
+                # no numbers
+            if any(char.isdigit() for char in word):
+                clean = False
+
+                # at least two characters but no more than 10
+            if len(word) < 2 or len(word) > 10:
+                clean = False
+
+            if not re.match(r'^\w+$', word):
+                clean = False
+
+            if clean:
+                try:
+                    clean_words.append(word.lower())
+                except UnicodeEncodeError:
+                    print(".")
+
+    return clean_words
