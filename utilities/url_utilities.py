86acad544c16bd754e93f729f50aa18fdd9551df
Shambhavi <reply2112@gmail.com>
2018-09-03 15:24:29 +0530

    Commit all files for the first time

--- utilities/url_utilities.py
+++ utilities/url_utilities.py
@@ -2,12 +2,12 @@ def load_urls_from_files(file_path: str):
     # ToDo: Add code to read the URL file.
     pass
 
+
 def load_page(url: str):
     # ToDo: Add code to read each URL.
     pass
 
+
 def scrape_page(page_contents: str):
     # ToDo: Analyze text.
     pass
-
-
